0954e664badd49fc09988133a88b642d3b3a2717
JeeIn Lee <jinny.l.dev@gmail.com>
2023-08-30 16:06:02 +0900

    [feat] 회원가입/로그인 기능 구현 (#23)
    * chore: build.gradle에 webflux 의존성 추가
    
    * chore: application.yml에 OAuth 관련 내용 추가
    
    * feat: OAuth yml 설정 파일과 바인딩되는 OAuthProperties 객체 구현
    
    * feat: OAuthRegistration 객체 및 OauthProperties를 OauthRegistration으로 변환하는 유틸 클래스 구현
    
    * feat: Oauth 서버별 유저 정보와 매핑되는 클래스 구현
    
    * feat: Oauth 정보를 저장하는 Repository 클래스 및 해당 클래스를 Bean으로 등록하는 Config 생성
    
    * feat: Oauth 서버에 유저 정보를 요청하는 기능 구현
    
    * feat: OAuth 로그인 테스트용 html 파일 생성
    
    * chore: build.gradle에 jwt 라이브러리 의존성 추가
    
    * chore: jwt 설정 yml 파일 추가
    
    * feat: JWT를 생성하는 유틸 클래스 구현
    
    * feat: JWT 토큰을 발급하는 기능 구현
    
    * chore: JPA 로깅 레벨 설정
    
    * feat: 회원가입을 위한 SignInType Entity 및 Repository 구현
    
    * feat: 회원가입을 위한 Member Entity 및 Repository 구현
    
    * feat: 회원가입을 위한 Address Entity 및 Repository 구현
    
    * feat: 회원가입을 위한 MemberAddress Entity 및 Repository 구현
    
    * feat: SignInType, Member, MemberAddress간 연간관계 추가
    
    * feat: 로그인 기능 구현
    
    * chore: jpa sql 터미널 출력 기능 설정
    
    * refactor: 깃허브 닉네임 값 요청 시 name -> login 값으로 변경
    
    name인 경우 null 값일 수 있어 필수 값인 login으로 변경
    
    * feat: github private 이메일 받아오는 메소드 구현
    
    * feat: OAuth yml 정보 수정
    
    * refactor: 양방향 관계 엔티티 -> 단방향으로 수정
    
    * rename: DTO inner class 통합으로 인한 클래스 삭제
    
    * refactor: MemberAddress 조회 반환 타입 Optional로 변경
    
    * refactor: member 정보를 담은 inner class 추가
    
    * style: 코드 스타일 적용
    
    * rename: api 하위로 도메인 디렉토리 변경
    
    * style: 주석 제거
    
    * feat: Github 이메일 요청 시 null 값 검증 추가, Enum에서 provider 검증
    
    * refactor: signUp 메소드 삭제
    
    ---------
    
    Co-authored-by: tmdqja0705@naver.com <tmdqja0705@naver.com>

---
+++ be/src/main/java/kr/codesquad/secondhand/api/member/service/MemberService.java
@@ -0,0 +1,60 @@
+package kr.codesquad.secondhand.api.member.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import kr.codesquad.secondhand.api.jwt.domain.Jwt;
+import kr.codesquad.secondhand.api.jwt.service.JwtService;
+import kr.codesquad.secondhand.api.member.domain.Member;
+import kr.codesquad.secondhand.api.member.domain.MemberAddress;
+import kr.codesquad.secondhand.api.member.domain.SignInType;
+import kr.codesquad.secondhand.api.member.dto.OAuthSignInResponse;
+import kr.codesquad.secondhand.api.member.repository.MemberAddressRepositoryImpl;
+import kr.codesquad.secondhand.api.member.repository.MemberRepository;
+import kr.codesquad.secondhand.api.member.repository.SignInTypeRepositoryImpl;
+import kr.codesquad.secondhand.api.oauth.domain.OAuthProfile;
+import kr.codesquad.secondhand.api.oauth.service.OAuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final OAuthService oAuthService;
+    private final JwtService jwtService;
+
+    private final SignInTypeRepositoryImpl signInTypeRepository;
+    private final MemberRepository memberRepository;
+    private final MemberAddressRepositoryImpl memberAddressRepository;
+
+    @Transactional
+    public OAuthSignInResponse signInOrSignUp(String providerName, String authorizationCode) {
+        OAuthProfile oAuthProfile = oAuthService.requestOauthProfile(providerName, authorizationCode);
+        SignInType signInType = signInTypeRepository.findByProvider(
+                providerName); // TODO: 메모리 저장소에 캐싱해두고 사용할 수 있게 개선하는 방법 고민중
+        Member member = oAuthProfile.toMember(signInType);
+
+        // DB에 회원 저장 유무 판단
+        Optional<Member> findMember = memberRepository.findBySignInTypeIdAndEmail(
+                member.getSignInType().getId(),
+                member.getEmail()
+        );
+
+        if (findMember.isEmpty()) {
+            memberRepository.save(member);
+            return signIn(member);
+        }
+        return signIn(findMember.get());
+    }
+
+    private OAuthSignInResponse signIn(Member member) {
+        Jwt jwt = jwtService.issueJwt(member.getId());
+        Optional<List<MemberAddress>> memberAddress = memberAddressRepository.findAllByMemberId(member.getId());
+        if (memberAddress.isPresent()) {
+            return OAuthSignInResponse.of(jwt, memberAddress.get(), member);
+        }
+        return OAuthSignInResponse.of(jwt, new ArrayList<>(), member);
+    }
+}
