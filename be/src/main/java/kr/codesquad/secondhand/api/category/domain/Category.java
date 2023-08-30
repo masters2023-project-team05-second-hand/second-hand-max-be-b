4d4bd4096a4d1adaac20f425df2af74fe07b528c
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-08-30 16:56:57 +0900

    feat: 카테고리 조회 entity, jpa repository, response dto 구현

---
+++ be/src/main/java/kr/codesquad/secondhand/api/category/domain/Category.java
@@ -0,0 +1,20 @@
+package kr.codesquad.secondhand.api.category.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
