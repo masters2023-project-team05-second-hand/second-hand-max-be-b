59c05db29133b925083be31adf04598aaea59485
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-08-30 14:30:30 +0900

    refactor: MemberAddress 조회 반환 타입 Optional로 변경

--- be/src/main/java/kr/codesquad/secondhand/member/repository/MemberAddressRepositoryImpl.java
+++ be/src/main/java/kr/codesquad/secondhand/member/repository/MemberAddressRepositoryImpl.java
@@ -1,10 +1,12 @@
 package kr.codesquad.secondhand.member.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import kr.codesquad.secondhand.member.domain.MemberAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberAddressRepositoryImpl extends JpaRepository<MemberAddress, Long> {
 
-    List<MemberAddress> findAllByMemberId(Long memberId);
+    Optional<List<MemberAddress>> findAllByMemberId(Long memberId);
 }
