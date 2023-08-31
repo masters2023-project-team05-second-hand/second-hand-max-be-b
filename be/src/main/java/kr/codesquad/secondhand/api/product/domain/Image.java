9e5a5e2f6bfdaa73e81818619df8c72050c37d26
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-08-31 15:52:36 +0900

    feat: 상품 등록에 필요한 엔티티 구현

---
+++ be/src/main/java/kr/codesquad/secondhand/api/product/domain/Image.java
@@ -0,0 +1,28 @@
+package kr.codesquad.secondhand.api.product.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private String url;
+}
--- be/src/main/java/kr/codesquad/secondhand/api/product/domain/Product.java
+++ be/src/main/java/kr/codesquad/secondhand/api/product/domain/Product.java
@@ -1,4 +1,69 @@
 package kr.codesquad.secondhand.api.product.domain;
 
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import kr.codesquad.secondhand.api.category.domain.Category;
+import kr.codesquad.secondhand.api.member.domain.Address;
+import kr.codesquad.secondhand.api.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "status_id")
+    private Status status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private String title;
+    private String content;
+    private Long price;
+    @CreationTimestamp
+    private Date createdTime;
+    private String thumbnailImgUrl;
+
+    @Builder
+    public Product(Member member, Status status, Address address, Category category, String title,
+                   String content,
+                   Long price, Date createdTime, String thumbnailImgUrl) {
+        this.member = member;
+        this.status = status;
+        this.address = address;
+        this.category = category;
+        this.title = title;
+        this.content = content;
+        this.price = price;
+        this.createdTime = createdTime;
+        this.thumbnailImgUrl = thumbnailImgUrl;
+    }
 }
