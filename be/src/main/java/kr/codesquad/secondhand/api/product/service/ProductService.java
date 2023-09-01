8965d719dd4c64bd5ae94246f223dfb04cba121f
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-09-01 11:39:01 +0900

    feat: ImageService 추가에 따른 코드 분리, 수정 메소드 구현

--- be/src/main/java/kr/codesquad/secondhand/api/product/service/ProductService.java
+++ be/src/main/java/kr/codesquad/secondhand/api/product/service/ProductService.java
@@ -1,32 +1,26 @@
 package kr.codesquad.secondhand.api.product.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.ObjectMetadata;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import kr.codesquad.secondhand.api.category.domain.Category;
 import kr.codesquad.secondhand.api.category.repository.CategoryRepositoryImpl;
 import kr.codesquad.secondhand.api.member.domain.Address;
 import kr.codesquad.secondhand.api.member.domain.Member;
 import kr.codesquad.secondhand.api.member.repository.AddressRepositoryImpl;
 import kr.codesquad.secondhand.api.member.service.MemberService;
-import kr.codesquad.secondhand.api.product.domain.Image;
 import kr.codesquad.secondhand.api.product.domain.Product;
 import kr.codesquad.secondhand.api.product.domain.ProductStatus;
 import kr.codesquad.secondhand.api.product.dto.ProductCreateRequest;
 import kr.codesquad.secondhand.api.product.dto.ProductCreateResponse;
-import kr.codesquad.secondhand.api.product.repository.ImageRepository;
+import kr.codesquad.secondhand.api.product.dto.ProductModifyRequest;
 import kr.codesquad.secondhand.api.product.repository.ProductRepository;
 import kr.codesquad.secondhand.api.product.repository.StatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -38,7 +32,7 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final AddressRepositoryImpl addressRepository;
     private final CategoryRepositoryImpl categoryRepository;
-    private final ImageRepository imageRepository;
+    private final ImageService imageService;
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
@@ -46,7 +40,7 @@ public class ProductService {
     public ProductCreateResponse save(ProductCreateRequest productCreateRequest, Long memberId) throws IOException {
         //  TODO 썸네일 리사이징 기능 구현
         // 임시 썸네일 이미지
-        List<URL> imageUrls = uploadMultiImagesToS3(productCreateRequest.getImages());
+        List<URL> imageUrls = imageService.uploadMultiImagesToS3(productCreateRequest.getImages());
         URL thumbnailImgUrl = imageUrls.get(0);
         Member seller = memberService.getMemberReferenceById(memberId);
 //        id가 아닌 type으로 조회할 경우 쿼리가 발생한다. (쿼리 메소드가 아닌 인터페이스에 추가한 메소드라서?)
@@ -56,33 +50,17 @@ public class ProductService {
         Category category = categoryRepository.getReferenceById(productCreateRequest.getCategoryId());
         Product product = productCreateRequest.toEntity(seller, status, address, category, thumbnailImgUrl);
         productRepository.save(product);
-        List<Image> images = imageUrlsToImage(imageUrls, product);
-        imageRepository.saveAll(images);
+        imageService.saveAll(imageUrls, product);
         return new ProductCreateResponse(product.getId());
     }
 
-
-    // TODO: 예외 처리: 파일 없을 때, 파일 확장자가 이미지가 아닐 때(처리하면 for문 stream으로 수정)
-    private List<URL> uploadMultiImagesToS3(List<MultipartFile> multipartFiles) throws IOException {
-        List<URL> urls = new ArrayList<>();
-        for (MultipartFile multipartFile : multipartFiles) {
-            urls.add(uploadSingleImageToS3(multipartFile));
-        }
-        return urls;
-    }
-
-    private URL uploadSingleImageToS3(MultipartFile multipartFile) throws IOException {
-        String uuid = UUID.randomUUID().toString(); // 이미지 이름 중복 방지를 위한 고유한 이미지 이름 생성
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(multipartFile.getSize());
-        metadata.setContentType(multipartFile.getContentType());
-        amazonS3.putObject(bucket, uuid, multipartFile.getInputStream(), metadata);
-        return amazonS3.getUrl(bucket, uuid);
-    }
-
-    private List<Image> imageUrlsToImage(List<URL> imageUrls, Product product) {
-        return imageUrls.stream()
-                .map(imageUrl -> new Image(product, imageUrl))
-                .collect(Collectors.toUnmodifiableList());
+    @Transactional
+    public void modify(Long productId, ProductModifyRequest productModifyRequest) throws IOException {
+        Product product = productRepository.findById(productId).orElseThrow();
+        URL thumbnailImgUrl = imageService.updateImageUrls(product, productModifyRequest.getNewImages(),
+                productModifyRequest.getDeletedImgIds());
+        Address address = addressRepository.getReferenceById(productModifyRequest.getAddressId());
+        Category category = categoryRepository.getReferenceById(productModifyRequest.getCategoryId());
+        product.updateProduct(productModifyRequest, address, category, thumbnailImgUrl);
     }
 }
