aec96d6c66f723e82b8b5b066c918d0f5c836ace
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-09-04 18:17:01 +0900

    style: 필드 한칸 띄우기

--- be/src/main/java/kr/codesquad/secondhand/api/member/dto/MemberAddressModifyRequest.java
+++ be/src/main/java/kr/codesquad/secondhand/api/member/dto/MemberAddressModifyRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberAddressModifyRequest {
+
     private List<Long> addressIds;
 
     public MemberAddressModifyRequest(List<Long> addressIds) {
