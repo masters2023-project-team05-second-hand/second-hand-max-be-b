ef436cce98525ccf5afd6b3ac785734d82684b82
jinny-l <jinny.l.dev@gmail.com>
2023-09-06 21:51:58 +0900

    style: 에러 메시지 오타 수정

--- be/src/main/java/kr/codesquad/secondhand/api/category/exception/InvalidCategoryIdException.java
+++ be/src/main/java/kr/codesquad/secondhand/api/category/exception/InvalidCategoryIdException.java
@@ -2,7 +2,7 @@ package kr.codesquad.secondhand.api.category.exception;
 
 public class InvalidCategoryIdException extends CategoryException {
 
-    private static final String ERROR_MESSAGE = "존재하지 않는 카테고리 Id 입니다.";
+    private static final String ERROR_MESSAGE = "존재하지 않는 Category Id 입니다.";
 
     public InvalidCategoryIdException() {
         super(ERROR_MESSAGE);
