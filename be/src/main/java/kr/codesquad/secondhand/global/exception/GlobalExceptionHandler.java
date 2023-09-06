b336995539eeeb5b70f851921bc5a3b93e8649a1
jinny-l <jinny.l.dev@gmail.com>
2023-09-06 21:50:46 +0900

    feat: OAuth 관련 예외처리
    * Provider Name이 유효하지 않을 경우
    * Oauth 서버로부터 유효한 응답을 받지 않았을 경우

--- be/src/main/java/kr/codesquad/secondhand/global/exception/GlobalExceptionHandler.java
+++ be/src/main/java/kr/codesquad/secondhand/global/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.function.client.WebClientException;
 
 @Slf4j
 @RestControllerAdvice
@@ -16,7 +17,15 @@ public class GlobalExceptionHandler {
     public ErrorResponseEntity handleException(Exception e) {
         log.error(e.getMessage());
         e.printStackTrace();
-        return new ErrorResponseEntity("[ERROR] 서버 오류입니다.");
+        return new ErrorResponseEntity("서버 오류입니다.");
+    }
+
+    @ExceptionHandler(WebClientException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponseEntity handleWebClientException(WebClientException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
+        return new ErrorResponseEntity("Oauth 서버 관련 오류입니다.");
     }
 
     @ExceptionHandler(CategoryException.class)
