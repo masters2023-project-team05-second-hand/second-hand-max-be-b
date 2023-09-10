47877dfc5c29a06f80f7baf147d6150fdd56dd07
jinny-l <jinny.l.dev@gmail.com>
2023-09-10 21:21:50 +0900

    feat: Member 도메인 Request DTO valid 처리

--- be/src/main/java/kr/codesquad/secondhand/api/product/dto/ProductStatusUpdateRequest.java
+++ be/src/main/java/kr/codesquad/secondhand/api/product/dto/ProductStatusUpdateRequest.java
@@ -1,10 +1,12 @@
 package kr.codesquad.secondhand.api.product.dto;
 
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class ProductStatusUpdateRequest {
 
+    @NotNull(message = "업데이트할 상품 상태 정보가 없습니다.")
     private Integer statusId;
 
 }
