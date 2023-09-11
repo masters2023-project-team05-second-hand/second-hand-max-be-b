0e8887939ba128ffeb6740d083b8eed153983e77
jinny-l <jinny.l.dev@gmail.com>
2023-09-11 21:55:55 +0900

    feat: 커스텀 예외를 CustomRuntimeException과 CustomException으로 분리

--- be/src/main/java/kr/codesquad/secondhand/api/product/exception/ProductException.java
+++ be/src/main/java/kr/codesquad/secondhand/api/product/exception/ProductException.java
@@ -1,8 +1,8 @@
 package kr.codesquad.secondhand.api.product.exception;
 
-import kr.codesquad.secondhand.global.exception.CustomException;
+import kr.codesquad.secondhand.global.exception.CustomRuntimeException;
 
-public class ProductException extends CustomException {
+public class ProductException extends CustomRuntimeException {
 
     private static final String ERROR_MESSAGE = "상품 관련 에러입니다.";
 
