e577278932aeb3e199b8a0a532f50b5d7df868ae
JeeIn Lee <jinny.l.dev@gmail.com>
2023-09-11 11:44:23 +0900

    [feat] request dto 유효성 검증 기능 구현 (#83)
    * chore: build.gradle에 validation 의존성 추가
    
    * feat: Member 도메인 Request DTO valid 처리
    
    * feat: Member 도메인 Request DTO valid 처리
    
    * feat: valid 관련 예외 처리

--- be/src/main/java/kr/codesquad/secondhand/api/product/dto/ProductUpdateRequest.java
+++ be/src/main/java/kr/codesquad/secondhand/api/product/dto/ProductUpdateRequest.java
@@ -1,17 +1,28 @@
 package kr.codesquad.secondhand.api.product.dto;
 
 import java.util.List;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 public class ProductUpdateRequest {
 
+    @NotNull(message = "등록할 상품의 카테고리 정보가 비어 있습니다.")
     private final Long categoryId;
+
+    @NotNull(message = "등록할 상품의 동네 정보가 비어 있습니다.")
     private final Long addressId;
+
+    @NotEmpty(message = "제목이 비어있습니다.")
     private final String title;
+
+    @NotEmpty(message = "내용이 비어있습니다.")
     private final String content;
+
     private final Long price;
+    // TODO Multipart는 Bean Validation이 안먹혀서 Spring이 제공하는 다른 방법이 있는지 확인 필요, 아님 직접 검증하는 코드 구현
     private final List<MultipartFile> newImages;
     private final List<Long> deletedImgIds;
 
