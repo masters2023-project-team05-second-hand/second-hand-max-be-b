3d5c0897b19661f956e3485a41ad5757306941a3
jinny-l <jinny.l.dev@gmail.com>
2023-09-13 22:39:59 +0900

    feat: CategoryException의 기본 에러 메시지 추가

--- be/src/main/java/kr/codesquad/secondhand/api/category/exception/CategoryException.java
+++ be/src/main/java/kr/codesquad/secondhand/api/category/exception/CategoryException.java
@@ -4,6 +4,12 @@ import kr.codesquad.secondhand.global.exception.CustomRuntimeException;
 
 public class CategoryException extends CustomRuntimeException {
 
+    private static final String ERROR_MESSAGE = "카테고리 관련 오류입니다.";
+
+    public CategoryException() {
+        super(ERROR_MESSAGE);
+    }
+
     public CategoryException(String message) {
         super(message);
     }
