cf50200c8c4db68516782399536f95a0011fed22
JeeIn Lee <jinny.l.dev@gmail.com>
2023-09-14 11:37:46 +0900

    [refactor] address, category, jwt 도메인 리팩토링 (#109)
    * refactor: Address Dto 디렉토리를 Member 하위에서 Address 도메인 하위로 이동
    
    * refactor: CategoryReadResponse 생성자 접근 제어자를 private으로 변경
    
    * style: 주석 추가
    
    * feat: CategoryException의 기본 에러 메시지 추가
    
    * refactor: Entity에서 @AllArgsConstructor 생성자 제거
    
    * refactor: CategoryReadResponse의 정적 팩토리 내부 로직을 빌더 패턴으로 변경

--- be/src/main/java/kr/codesquad/secondhand/api/category/dto/CategoryReadResponse.java
+++ be/src/main/java/kr/codesquad/secondhand/api/category/dto/CategoryReadResponse.java
@@ -3,6 +3,7 @@ package kr.codesquad.secondhand.api.category.dto;
 import java.util.List;
 import java.util.stream.Collectors;
 import kr.codesquad.secondhand.api.category.domain.Category;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -12,19 +13,28 @@ public class CategoryReadResponse {
     private final String name;
     private final String imgUrl;
 
-    public CategoryReadResponse(Long id, String name, String imgUrl) {
+    @Builder
+    private CategoryReadResponse(Long id, String name, String imgUrl) {
         this.id = id;
         this.name = name;
         this.imgUrl = imgUrl;
     }
 
     public static CategoryReadResponse from(Category category) {
-        return new CategoryReadResponse(category.getId(), category.getName(), category.getImgUrl());
+        return CategoryReadResponse.builder()
+                .id(category.getId())
+                .name(category.getName())
+                .imgUrl(category.getImgUrl())
+                .build();
     }
 
     public static List<CategoryReadResponse> from(List<Category> categories) {
         return categories.stream()
-                .map(category -> new CategoryReadResponse(category.getId(), category.getName(), category.getImgUrl()))
+                .map(category -> CategoryReadResponse.builder()
+                        .id(category.getId())
+                        .name(category.getName())
+                        .imgUrl(category.getImgUrl())
+                        .build())
                 .collect(Collectors.toUnmodifiableList());
     }
 }
