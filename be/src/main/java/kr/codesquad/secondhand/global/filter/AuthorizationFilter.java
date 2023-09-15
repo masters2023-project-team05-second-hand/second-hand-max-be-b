84fcde9bd20161159a14c6d3bca22a6ad9490da6
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-09-15 10:16:24 +0900

    chore: statuses 조회 요청 화이트리스트 추가

--- be/src/main/java/kr/codesquad/secondhand/global/filter/AuthorizationFilter.java
+++ be/src/main/java/kr/codesquad/secondhand/global/filter/AuthorizationFilter.java
@@ -34,7 +34,8 @@ public class AuthorizationFilter implements Filter {
             "/",
             "/api/addresses",
             "/api/categories",
-            "/api/products*"
+            "/api/products*",
+            "/api/statuses"
     };
 
     private static final String[] POST_WHITE_LIST = new String[]{
