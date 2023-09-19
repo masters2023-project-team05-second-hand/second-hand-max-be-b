69f0aa768df1214b8d21cfb1c943497856e9d05d
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-09-20 00:21:09 +0900

    refactor: 스케줄링 로직 수정

--- be/src/main/java/kr/codesquad/secondhand/api/product/service/StatsSchedulerFacadeService.java
+++ be/src/main/java/kr/codesquad/secondhand/api/product/service/StatsSchedulerFacadeService.java
@@ -1,10 +1,11 @@
 package kr.codesquad.secondhand.api.product.service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import kr.codesquad.secondhand.api.member.domain.MemberStatLog;
+import kr.codesquad.secondhand.api.member.domain.MemberStatLogRedis;
+import kr.codesquad.secondhand.api.member.service.MemberStatLogRedisService;
 import kr.codesquad.secondhand.api.member.service.MemberStatLogService;
 import kr.codesquad.secondhand.api.product.domain.ProductStat;
 import lombok.RequiredArgsConstructor;
@@ -16,25 +17,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class StatsSchedulerFacadeService {
 
-    private final StatService statService;
     private final ProductStatService productStatService;
     private final MemberStatLogService memberStatLogService;
+    private final MemberStatLogRedisService memberStatLogRedisService;
 
     @Transactional
-    @Scheduled(cron = "0 0/3 * * * ?")
+    @Scheduled(cron = "0 0/1 * * * ?")
     public void backUpProductStat() {
-        Map<String, Set<Long>> keys = statService.getKeys();
+        Set<Long> productKeys = productStatService.getProductKeys();
 
-        Set<Long> productKeys = keys.get("product");
         List<ProductStat> productStats = productKeys.stream()
-                .map(statService::findProductStat)
+                .map(productStatService::findProductStat)
                 .collect(Collectors.toUnmodifiableList());
-
-        Set<Long> memberKeys = keys.get("member");
-        List<MemberStatLog> memberStatLogs = memberKeys.stream()
-                .map(statService::findMemberStatLog)
+        List<MemberStatLogRedis> memberStatLogRedis = memberStatLogRedisService.findAll();
+        List<MemberStatLog> memberStatLogs = memberStatLogRedis.stream()
+                .map(MemberStatLog::from)
                 .collect(Collectors.toUnmodifiableList());
-        statService.flushAllRedis();
+        productStatService.flushAllRedis();
         productStatService.saveAll(productStats);
         memberStatLogService.saveAll(memberStatLogs);
     }
