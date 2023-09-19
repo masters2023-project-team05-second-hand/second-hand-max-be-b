a8d5732373ed30c5518fa5516317b1a89281939e
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-09-20 00:19:31 +0900

    feat: MemberStatLog Entity 정적 팩토리 메소드 생성

--- be/src/main/java/kr/codesquad/secondhand/api/member/domain/MemberStatLog.java
+++ be/src/main/java/kr/codesquad/secondhand/api/member/domain/MemberStatLog.java
@@ -5,23 +5,32 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class MemberStatLog {
 
     @Id
     @Column(name = "member_id")
     private Long id;
-
     private String viewedProductIds;
     private String wishedProductIds;
 
     @Builder
-    public MemberStatLog(Long id, String viewedProductIds, String wishedProductIds) {
+    private MemberStatLog(Long id, String viewedProductIds, String wishedProductIds) {
         this.id = id;
         this.viewedProductIds = viewedProductIds;
         this.wishedProductIds = wishedProductIds;
+    }
+
+    public static MemberStatLog from(MemberStatLogRedis memberStatLogRedis){
+        return MemberStatLog.builder()
+                .id(memberStatLogRedis.getId())
+                .viewedProductIds(memberStatLogRedis.getViewedProductIds())
+                .wishedProductIds(memberStatLogRedis.getWishedProductIds())
+                .build();
     }
 }
