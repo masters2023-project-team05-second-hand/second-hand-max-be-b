b8c6f8009075239ebcf6f1e8ec3903a681a66601
jinny-l <jinny.l.dev@gmail.com>
2023-09-26 23:49:48 +0900

    feat: 웹소켓 Config 작성

---
+++ be/src/main/java/kr/codesquad/secondhand/api/chat/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package kr.codesquad.secondhand.api.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS(); // APIC으로 STOMP 테스트 시 해당 옵션 주석 처리 필요
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/sub");
+    }
+}
